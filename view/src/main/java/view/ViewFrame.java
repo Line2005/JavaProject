5754d25786cea99da278d0e564e413cce1727c09
Daniela-Nancy23 <daninancyyy@gmail.com>
2023-11-15 13:45:50 +0100

    Commit changes of the module View by NGUINSOM SOWE Daniela Nancy.

---
+++ view/src/main/java/view/ViewFrame.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import contract.IController;
+import contract.IModel;
+
+/**
+ * The Class ViewFrame.
+ *
+ * @author Jean-Aymeric Diet
+ */
+class ViewFrame extends JFrame  {
+
+}
---
+++ view/src/main/java/view/ViewPanel.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+/**
+ * The Class ViewPanel.
+ *
+ * @author Jean-Aymeric Diet
+ */
+class ViewPanel extends JPanel  {
+
+
+}
