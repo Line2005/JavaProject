cec33e30fa6eb7ff4859b34a3fd121421f97d224
Daniela-Nancy23 <142411441+Daniela-Nancy23@users.noreply.github.com>
2023-11-15 21:47:40 +0100

    View
    Commit Changes of the module View by NGUINSOM SOWE Daniela Nancy.

---
+++ view/src/main/java/view/ViewFrame.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import contract.IController;
+import contract.IModel;
+
+/**
+ * The Class ViewFrame.
+ *
+ * @author NGUINSOM SOWE Daniela Nancy
+ */
+class ViewFrame extends JFrame  {
+
+}
---
+++ view/src/main/java/view/ViewPanel.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+/**
+ * The Class ViewPanel.
+ *
+ * @author NGUINSOM SOWE Daniela Nancy
+ */
+class ViewPanel extends JPanel  {
+
+
+}
